994a9c9d94d8a0f9a918c5b08176b7e096090584
richardandreas <richard.stierlin@housecallpro.com>
2022-06-11 18:49:58 -0300

    Rendering cards

--- src/components/Views/Search/DogCard.js
+++ src/components/Views/Search/DogCard.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Card, Col, Image } from "antd";
+
+const { Meta } = Card;
 
 const DogCard = ({ url }) => {
-  return <p>{url}</p>;
+  const getBreedAndSubbreedFromUrl = (url) => {
+    const splittedUrl = url.split("/");
+    const breedWithSubbreed = splittedUrl[splittedUrl.length - 2];
+
+    return breedWithSubbreed.split("-");
+  };
+
+  const [breed, subbreed] = getBreedAndSubbreedFromUrl(url);
+
+  return (
+    <Col>
+      <Card
+        hoverable
+        style={{ width: 240, margin: "20px 0" }}
+        cover={<Image src={url} />}
+      >
+        <Meta title={breed} description={subbreed} />
+      </Card>
+    </Col>
+  );
 };
 
 export default DogCard;
