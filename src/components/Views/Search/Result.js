28ab7a30cd6d6e03cbf62820a6b6eb253a4f4f38
richardandreas <richard.stierlin@housecallpro.com>
2022-12-14 22:55:05 -0300

    Use Promise.all to fetch all routes

--- src/components/Views/Search/Result.js
+++ src/components/Views/Search/Result.js
@@ -2,30 +2,15 @@ import React, { useContext, useState } from "react";
 import { Pagination, Row, Col } from "antd";
 import { ContentContainer } from "../../Containers";
 import { SearchContext } from "../../Contexts";
-import { useDogApiMultiple } from "../../../hooks/useDogApi";
+import { useDogMultiApi } from "../../../hooks/useDogApi";
 import DogCard from "./DogCard";
 import Spinner from "./../../Spinner";
-import forEachObject from "../../../utils/forEachObject";
-import buildSearchUrls from "../../../utils/buildSearchUrls";
 import sortArray from "../../../utils/sortArray";
 
 const Result = () => {
   const { searchParams } = useContext(SearchContext);
   const [page, setPage] = useState(1);
-  const [data, loading] = useDogApiMultiple(
-    buildSearchUrls(searchParams),
-    searchParams
-  );
-
-  const dataMerger = (data) => {
-    let mergedArray = [];
-
-    forEachObject(data, (_key, value) => {
-      mergedArray = [...mergedArray, ...value];
-    });
-
-    return sortArray(mergedArray);
-  };
+  const [data, loading] = useDogMultiApi(searchParams);
 
   const searchTerm = () => {
     const searchTerm =
@@ -45,7 +30,7 @@ const Result = () => {
       </ContentContainer>
     );
 
-  const results = dataMerger(data);
+  const results = sortArray(data);
   const paginatedResults = results.slice(page * 12 - 12, page * 12);
 
   return (
